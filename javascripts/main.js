856a32e7894942ec7f4b08e2f1428e2ade20e8f3
zchaudhry11 <zchaudhry11@gmail.com>
2015-02-28 17:15:07 -0500

    tabs

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+$(document).ready(function() {
+    $('.tabs .tab-links a').on('click', function(e) {
+        var currentAttrValue = $(this).attr('href');
+
+    // Show/Hide Tabs
+    $('.tabs ' + currentAttrValue).show().siblings().hide();
+       // Change/remove current tab to active
+    $(this).parent('li').addClass('active').siblings().removeClass('active');
+        e.preventDefault();
+    });
+});
